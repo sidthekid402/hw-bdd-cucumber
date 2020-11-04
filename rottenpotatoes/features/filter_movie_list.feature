a0c06662583e78fe87c309446d524a3b22391722
Siddhant Rao <siddrao@berkeley.edu>
2020-11-04 02:58:50 +0000

    part1

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -24,10 +24,20 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check 'PG'
+  And I check 'R'
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck 'G'
+  And I uncheck 'PG-13'
   # enter step to "submit" the search form on the homepage
+  And I press 'submit'
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see 'PG'
+  And I should see 'R'
   # enter step(s) to ensure that other movies are not visible
+  And I should not see 'G'
+  And I should not see 'PG-13'
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: G, PG, PG-13, R
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -29,10 +29,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(',').each do |rating|
+      if (uncheck)
+          step "I uncheck \"#{rating}\""
+      else
+          step "I check \"#{rating}\""
+      end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(Movie.all.count).to eq all("table#movies tr").count
 end
